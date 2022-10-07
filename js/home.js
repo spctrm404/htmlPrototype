23825317f9f53283c383a5d001d3688c8d188da1
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2022-10-08 03:27:37 +0900

    Initial

---
+++ js/home.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    scrollUpBtn.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }, 1);
+});
+
+const scrollUpBtn = document.querySelector('#scrollUpBtn');
+const menuBar = document.querySelector('#menuBar');
+scrollUpBtn.addEventListener('click', (e) => {
+  menuBar.scrollIntoView({ behavior: 'smooth' });
+});
+
+const poster = document.querySelector('#posterContainer');
+const homeBtn = document.querySelector('#homeBtn');
+homeBtn.addEventListener('click', (e) => {
+  poster.scrollIntoView({ behavior: 'smooth' });
+});
+
+const posterLottie = document.getElementById('posterLottie');
+lottie.loadAnimation({
+  container: posterLottie,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/asset/front/front.json', // the path to the animation json
+});
